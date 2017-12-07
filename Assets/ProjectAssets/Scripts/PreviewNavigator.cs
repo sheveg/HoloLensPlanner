89775a9f06a0850c30b636176bc0d4a7e942222e
Fabian Kloos <kloos@in.tum.de>
2017-12-07 15:27:02 +0100

    Show Tile in Preview

---
+++ Assets/ProjectAssets/Scripts/PreviewNavigator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+
+public class PreviewNavigator : Singleton<PreviewNavigator> {
+
+    public RectTransform ObjectMenu;
+    public RectTransform ObjectPreview;
+    public RectTransform PreviewSpawnPoint;
+
+    //TODO: make scale logarithmic
+    public float scale;
+
+    private GameObject tilePreview;
+
+    public void Navigate(TileMenuObject tile)
+    {
+        ObjectMenu.gameObject.SetActive(false);
+        ObjectPreview.gameObject.SetActive(true);
+
+        tilePreview = Instantiate(tile.TileMesh, PreviewSpawnPoint.position, Quaternion.identity, PreviewSpawnPoint);
+        tilePreview.name = "TilePreview";
+        tilePreview = scaleObject(tilePreview);
+    }
+
+    public GameObject scaleObject(GameObject gameObject)
+    {
+        //TODO make sure object doesn't get bigger than view
+        gameObject.transform.localScale *= scale;
+        return gameObject;
+    }
+
+    public void backButton()
+    {
+        ObjectPreview.gameObject.SetActive(false);
+        ObjectMenu.gameObject.SetActive(true);
+
+        GameObject.Destroy(tilePreview);
+    }
+}
---
+++ Assets/ProjectAssets/Scripts/PreviewPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewPanel : MonoBehaviour {
+
+    public RectTransform selector;
+    public GameObject[] tiles;
+
+	// Use this for initialization
+	void Start () {
+        tiles = new GameObject[selector.transform.childCount];
+        for(int i = 0; i < selector.transform.childCount; i++)
+        {
+            tiles[i] = selector.transform.GetChild(i).transform.GetChild(0).gameObject;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
