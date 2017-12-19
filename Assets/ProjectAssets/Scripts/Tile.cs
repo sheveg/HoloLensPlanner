0663c27e961b83c27b0f717c29d20d86b93dc97a
Egor Shevdin <egor_sh@live.de>
2017-12-19 16:47:32 +0100

    tile template class

---
+++ Assets/ProjectAssets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloLensPlanner
+{
+    public class Tile : MonoBehaviour
+    {
+
+        public float Height;
+
+        public float Width;
+
+    }
+}
--- Assets/ProjectAssets/Scripts/TilesGeneration/TilesGenerator.cs
+++ Assets/ProjectAssets/Scripts/TilesGeneration/TilesGenerator.cs
@@ -2,140 +2,132 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TilesGenerator : MonoBehaviour {
-
-    public Transform Tile;
-
-    public Transform SpawnPoint;
-    public Transform DirectionPoint;
-
-    public float TileOffset = 0.01f;
-
-    public List<Transform> AllPoints = new List<Transform>();
-
-    private void Start()
+namespace HoloLensPlanner
+{
+    public class TilesGenerator : MonoBehaviour
     {
-        
-        SpawnPoint.forward = DirectionPoint.position - SpawnPoint.position;
 
-        float maxZ = 0f;
-        float maxX = 0f;
-        float minX = 0f;
-        float minZ = 0f;
+        public Transform Tile;
 
-        for (int i = 0; i < AllPoints.Count; i++)
+        public Transform SpawnPoint;
+        public Transform DirectionPoint;
+
+        public float TileOffset = 0.01f;
+
+        public List<Transform> AllPoints = new List<Transform>();
+
+        private IEnumerator Start()
         {
-            Vector3 localPosition = SpawnPoint.InverseTransformPoint(AllPoints[i].position);
 
-            if (localPosition.x > maxX)
-            {
-                maxX = localPosition.x;
-            }
-            else if (localPosition.x < minX)
-            {
-                minX = localPosition.x;
-            }
-                
+            SpawnPoint.forward = DirectionPoint.position - SpawnPoint.position;
 
-            if (localPosition.z > maxZ)
+            float maxZ = 0f;
+            float maxX = 0f;
+            float minX = 0f;
+            float minZ = 0f;
+
+            for (int i = 0; i < AllPoints.Count; i++)
             {
-                maxZ = localPosition.z;
+                Vector3 localPosition = SpawnPoint.InverseTransformPoint(AllPoints[i].position);
+
+                if (localPosition.x > maxX)
+                {
+                    maxX = localPosition.x;
+                }
+                else if (localPosition.x < minX)
+                {
+                    minX = localPosition.x;
+                }
+
+
+                if (localPosition.z > maxZ)
+                {
+                    maxZ = localPosition.z;
+                }
+                else if (localPosition.z < minZ)
+                {
+                    minZ = localPosition.z;
+                }
             }
-            else if (localPosition.z < minZ)
+
+            var tilePlaneVertex0 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            tilePlaneVertex0.transform.localScale = Vector3.one * 0.3f;
+            Vector3 vertexLocalPosition = new Vector3(minX, Tile.position.y, minZ);
+            tilePlaneVertex0.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
+            tilePlaneVertex0.name = "Vertex0";
+
+            var tilePlaneVertex1 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            tilePlaneVertex1.transform.localScale = Vector3.one * 0.3f;
+            vertexLocalPosition = new Vector3(maxX, Tile.position.y, maxZ);
+            tilePlaneVertex1.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
+            tilePlaneVertex1.name = "Vertex1";
+
+            var tilePlaneVertex2 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            tilePlaneVertex2.transform.localScale = Vector3.one * 0.3f;
+            vertexLocalPosition = new Vector3(minX, Tile.position.y, maxZ);
+            tilePlaneVertex2.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
+            tilePlaneVertex2.name = "Vertex2";
+
+            var tilePlaneVertex3 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            tilePlaneVertex3.transform.localScale = Vector3.one * 0.3f;
+            vertexLocalPosition = new Vector3(maxX, Tile.position.y, minZ);
+            tilePlaneVertex3.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
+            tilePlaneVertex3.name = "Vertex3";
+
+            GameObject tilePlane = new GameObject("TilePlane");
+            tilePlane.transform.position = (tilePlaneVertex0.transform.position + tilePlaneVertex1.transform.position + tilePlaneVertex2.transform.position + tilePlaneVertex3.transform.position) / 4f;
+            tilePlaneVertex0.transform.parent = tilePlane.transform;
+            tilePlaneVertex1.transform.parent = tilePlane.transform;
+            tilePlaneVertex2.transform.parent = tilePlane.transform;
+            tilePlaneVertex3.transform.parent = tilePlane.transform;
+
+
+            //Tile.forward = DirectionPoint.position - SpawnPoint.position;
+            //Tile.position = SpawnPoint.position + Tile.right * tileMesh.bounds.extents.x * Tile.localScale.x + Tile.forward * tileMesh.bounds.extents.z * Tile.localScale.z;
+            Mesh tileMesh = Tile.GetComponent<MeshFilter>().mesh;
+            float tileWidth = tileMesh.bounds.extents.x * Tile.localScale.x * 2f;
+            float tileHeight = tileMesh.bounds.extents.z * Tile.localScale.z * 2f;
+            int columns = Mathf.CeilToInt((tilePlaneVertex0.transform.position - tilePlaneVertex3.transform.position).magnitude / tileWidth);
+            int rows = Mathf.CeilToInt((tilePlaneVertex0.transform.position - tilePlaneVertex2.transform.position).magnitude / tileHeight);
+            tilePlaneVertex0.transform.forward = (tilePlaneVertex2.transform.position - tilePlaneVertex0.transform.position).normalized;
+            tilePlaneVertex0.transform.right = (tilePlaneVertex3.transform.position - tilePlaneVertex0.transform.position).normalized;
+            Vector3 startingPosition = tilePlaneVertex0.transform.position + tilePlaneVertex0.transform.right * tileWidth / 2f + tilePlaneVertex0.transform.forward * tileHeight / 2f;
+
+            Vector3 startOffset = tilePlaneVertex0.transform.InverseTransformPoint(SpawnPoint.position) * tilePlaneVertex0.transform.localScale.x;
+            float xOffset = tileWidth - Mathf.Repeat(startOffset.x, tileWidth);
+            float zOffset = tileHeight - Mathf.Repeat(startOffset.z, tileHeight);
+            Debug.Log(xOffset + " : " + zOffset);
+
+            startingPosition -= xOffset * tilePlaneVertex0.transform.right + zOffset * tilePlaneVertex0.transform.forward;
+
+
+            Debug.Log(rows + " : " + columns);
+
+
+
+            for (int i = 0; i < rows + 1; i++)
             {
-                minZ = localPosition.z;
+                for (int j = 0; j < columns + 1; j++)
+                {
+                    yield return new WaitForSeconds(1f);
+                    Vector3 offset = i * tileHeight * tilePlaneVertex0.transform.forward * (1 + TileOffset / tileHeight) + j * tileWidth * tilePlaneVertex0.transform.right * (1 + TileOffset / tileWidth);
+                    Transform tileCopy = Instantiate(Tile, startingPosition + offset, tilePlaneVertex0.transform.rotation);
+                    tileCopy.transform.parent = tilePlane.transform;
+                }
             }
         }
 
-        var tilePlaneVertex0 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        tilePlaneVertex0.transform.localScale = Vector3.one * 0.3f;
-        Vector3 vertexLocalPosition = new Vector3(minX, Tile.position.y, minZ);
-        tilePlaneVertex0.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
-        tilePlaneVertex0.name = "Vertex0";
-
-        var tilePlaneVertex1 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        tilePlaneVertex1.transform.localScale = Vector3.one * 0.3f;
-        vertexLocalPosition = new Vector3(maxX, Tile.position.y, maxZ);
-        tilePlaneVertex1.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
-        tilePlaneVertex1.name = "Vertex1";
-
-        var tilePlaneVertex2 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        tilePlaneVertex2.transform.localScale = Vector3.one * 0.3f;
-        vertexLocalPosition = new Vector3(minX, Tile.position.y, maxZ);
-        tilePlaneVertex2.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
-        tilePlaneVertex2.name = "Vertex2";
-
-        var tilePlaneVertex3 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        tilePlaneVertex3.transform.localScale = Vector3.one * 0.3f;
-        vertexLocalPosition = new Vector3(maxX, Tile.position.y, minZ);
-        tilePlaneVertex3.transform.position = SpawnPoint.TransformPoint(vertexLocalPosition);
-        tilePlaneVertex3.name = "Vertex3";
-
-        GameObject tilePlane = new GameObject("TilePlane");
-        tilePlane.transform.position = (tilePlaneVertex0.transform.position + tilePlaneVertex1.transform.position + tilePlaneVertex2.transform.position + tilePlaneVertex3.transform.position) / 4f;
-        tilePlaneVertex0.transform.parent = tilePlane.transform;
-        tilePlaneVertex1.transform.parent = tilePlane.transform;
-        tilePlaneVertex2.transform.parent = tilePlane.transform;
-        tilePlaneVertex3.transform.parent = tilePlane.transform;
-
-
-        //Tile.forward = DirectionPoint.position - SpawnPoint.position;
-        //Tile.position = SpawnPoint.position + Tile.right * tileMesh.bounds.extents.x * Tile.localScale.x + Tile.forward * tileMesh.bounds.extents.z * Tile.localScale.z;
-        Mesh tileMesh = Tile.GetComponent<MeshFilter>().mesh;
-        float tileWidth = tileMesh.bounds.extents.x * Tile.localScale.x * 2f;
-        float tileHeight = tileMesh.bounds.extents.z * Tile.localScale.z * 2f;
-        int columns = Mathf.CeilToInt((tilePlaneVertex0.transform.position - tilePlaneVertex3.transform.position).magnitude / tileWidth);
-        int rows = Mathf.CeilToInt((tilePlaneVertex0.transform.position - tilePlaneVertex2.transform.position).magnitude / tileHeight);
-        tilePlaneVertex0.transform.forward = (tilePlaneVertex2.transform.position - tilePlaneVertex0.transform.position).normalized;
-        tilePlaneVertex0.transform.right = (tilePlaneVertex3.transform.position - tilePlaneVertex0.transform.position).normalized;
-        Vector3 startingPosition = tilePlaneVertex0.transform.position + tilePlaneVertex0.transform.right * tileWidth / 2f +  tilePlaneVertex0.transform.forward * tileHeight / 2f;
-
-        Vector3 startOffset = tilePlaneVertex0.transform.InverseTransformPoint(SpawnPoint.position) * tilePlaneVertex0.transform.localScale.x;
-        float xOffset = tileWidth - Mathf.Repeat(startOffset.x, tileWidth);
-        float zOffset = tileHeight - Mathf.Repeat(startOffset.z, tileHeight);
-        Debug.Log(xOffset + " : " + zOffset);
-
-        startingPosition -= xOffset * tilePlaneVertex0.transform.right + zOffset * tilePlaneVertex0.transform.forward;
-        
-
-        Debug.Log(rows + " : " + columns);
-
-
-
-        for (int i = 0; i < rows+1; i++)
+        private void createTiles(RoomPlane plane, Transform spawnPoint, Transform directionPoint)
         {
-            for (int j = 0; j < columns+1; j++)
-            {
-                Vector3 offset = i * tileHeight * tilePlaneVertex0.transform.forward * (1+TileOffset / tileHeight) + j * tileWidth * tilePlaneVertex0.transform.right * (1 + TileOffset / tileWidth);
-                Transform tileCopy = Instantiate(Tile, startingPosition + offset, tilePlaneVertex0.transform.rotation);
-                tileCopy.transform.parent = tilePlane.transform;
-            }
+            // first create the copy so we do not mess with the original spawnpoint, we will destroy this object later on
+            GameObject spawnPointCopy = new GameObject("SpawnPointCopy");
+            spawnPointCopy.transform.position = spawnPoint.position;
+            spawnPointCopy.transform.rotation = spawnPoint.rotation;
+            spawnPointCopy.transform.localScale = spawnPoint.localScale;
+
+
         }
-
-
-
-        //Vector3 topLeftTilePosition = new Vector3(Tile.position.x - tileMesh.bounds.extents.x, Tile.position.y, Tile.position.z - tileMesh.bounds.extents.z);
-        //var topLeftTile = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        //topLeftTile.transform.position = topLeftTilePosition;
-        //topLeftTile.transform.localScale = Vector3.one * 0.1f;
-        //topLeftTile.name = "topLeftTile";
-
-        //Vector3 topRightTilePosition = new Vector3(Tile.position.x + tileMesh.bounds.extents.x, Tile.position.y, Tile.position.z - tileMesh.bounds.extents.z);
-        //var topRightTile = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        //topRightTile.transform.position = topRightTilePosition;
-        //topRightTile.transform.localScale = Vector3.one * 0.1f;
-        //topRightTile.name = "topRightTile";
-
-        //Vector3 bottomLeftTilePosition = new Vector3(Tile.position.x - tileMesh.bounds.extents.x, Tile.position.y, Tile.position.z + tileMesh.bounds.extents.z);
-        //var bottomLeftTile = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        //bottomLeftTile.transform.position = bottomLeftTilePosition;
-        //bottomLeftTile.transform.localScale = Vector3.one * 0.1f;
-        //bottomLeftTile.name = "bottomLeftTile";
-
-        //Vector3 bottomRightTilePosition = new Vector3(Tile.position.x + tileMesh.bounds.extents.x, Tile.position.y, Tile.position.z + tileMesh.bounds.extents.z);
-        //var bottomRightTile = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-        //bottomRightTile.transform.position = bottomRightTilePosition;
-        //bottomRightTile.transform.localScale = Vector3.one * 0.1f;
-        //bottomRightTile.name = "bottomRightTile";
     }
+
+
 }
