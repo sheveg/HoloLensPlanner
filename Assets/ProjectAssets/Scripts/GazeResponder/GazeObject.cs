dbf00802a135252f999401bf1f444823a227e8b8
Egor Shevdin <egor_sh@live.de>
2018-01-11 15:32:30 +0100

    TileDetailView texture load changed to rawimage for texturebutton

--- Assets/ProjectAssets/Scripts/GazeResponder/GazeObject.cs
+++ Assets/ProjectAssets/Scripts/GazeResponder/GazeObject.cs
@@ -8,7 +8,7 @@ namespace HoloLensPlanner.GazeResponse
     /// GazeObject transforms a standard gameObject into an object which responds to the HoloLens gaze. Depending on the <see cref="GazeResponder.OnFocus"/> variable
     /// the gameObject reacts with the corresponding event, e.g. it scales up when <see cref="FocusEvent.Scale"/> is selected.
     /// </summary>
-    [RequireComponent(typeof(Renderer))]
+    //[RequireComponent(typeof(Renderer))]
     public class GazeObject : GazeResponder
     {
         protected override void OnColor()
